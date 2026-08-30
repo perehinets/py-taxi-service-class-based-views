7a9843dfefe0b957a7fd71fa8cd2ba4ac559892a
Vasyl Perehinets <perehinets95@gmail.com>
2026-08-30 19:11:25 +0100

    solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("__debug__/", include(
+        "debug_toolbar.urls",
+        namespace="debug_toolbar"
+    )),
+]
